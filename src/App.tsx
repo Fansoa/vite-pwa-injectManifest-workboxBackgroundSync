6c43d0466fba7e45856a121e5a277be37a6305eb
fansoa <fverin.contactpro@gmail.com>
2024-11-08 16:20:05 +0100

    feat(App): integration of a button allowing you to execute a post request with fetch on localhost:3000

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import appLogo from '/favicon.svg'
 import PWABadge from './PWABadge.tsx'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={appLogo} className="logo" alt="vite-pwa-injectManifest-workboxBackgroundSync logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>vite-pwa-injectManifest-workboxBackgroundSync</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <form onSubmit={async (e) => {
+        e.preventDefault()
+        return await fetch(
+          "http://localhost:3000", 
+          {
+            method: 'POST',
+            headers: [
+              ["Content-Type", "application/json"],
+            ],
+            body: JSON.stringify({
+              toto: "toto"
+            })
+          }
+        )
+      }}>
+        <button type='submit'/>
+      </form>
       <PWABadge />
     </>
   )
