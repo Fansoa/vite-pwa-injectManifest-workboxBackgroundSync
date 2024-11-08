c342b954a8be094d1f2feb3f10994794f19541d2
fansoa <fverin.contactpro@gmail.com>
2024-11-08 17:31:18 +0100

    feat(service workers): queue up failed requests and retry them when future sync events are fired

--- src/sw.ts
+++ src/sw.ts
@@ -1,6 +1,7 @@
 /// <reference lib="webworker" />
 import { cleanupOutdatedCaches, createHandlerBoundToURL, precacheAndRoute } from 'workbox-precaching'
 import { NavigationRoute, registerRoute } from 'workbox-routing'
+import { Queue } from 'workbox-background-sync';
 
 declare let self: ServiceWorkerGlobalScope
 
@@ -26,3 +27,28 @@ registerRoute(new NavigationRoute(
   createHandlerBoundToURL('index.html'),
   { allowlist },
 ))
+
+const queue = new Queue('myQueueName');
+
+self.addEventListener('fetch', (event) => {
+  console.log("🚀 ~ self.addEventListener ~ event:", event)
+  // Add in your own criteria here to return early if this
+  // isn't a request that should use background sync.
+  if (event.request.method !== 'POST') {
+    return;
+  }
+
+  const bgSyncLogic = async () => {
+    try {
+      const response = await fetch(event.request.clone());
+      console.log("🚀 ~ bgSyncLogic ~ response:", response)
+      return response;
+    } catch (error) {
+      await queue.pushRequest({request: event.request});
+      return error;
+    }
+  };
+
+  // @ts-ignore
+  event.respondWith(bgSyncLogic());
+})
